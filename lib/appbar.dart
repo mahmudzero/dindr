984129ece84d4bacc0b7b36178927a3173d885e1
RomanBogatikov <ivaroman2000@gmail.com>
2019-06-01 14:51:55 -0400

    materialicons navbar

---
+++ lib/appbar.dart
@@ -0,0 +1,22 @@
+// import 'package:flutter_web/material.dart';
+
+// AppBar(
+//   title: Text('My Fancy Dress'),
+//   actions: <Widget>[
+//     IconButton(
+//       icon: Icon(Icons.playlist_play),
+//       tooltip: 'Air it',
+//       onPressed: _airDress,
+//     ),
+//     IconButton(
+//       icon: Icon(Icons.playlist_add),
+//       tooltip: 'Restitch it',
+//       onPressed: _restitchDress,
+//     ),
+//     IconButton(
+//       icon: Icon(Icons.playlist_add_check),
+//       tooltip: 'Repair it',
+//       onPressed: _repairDress,
+//     ),
+//   ],
+// )
--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,116 @@
 import 'package:flutter_web/material.dart';
+// import './appbar.dart';
 
 void main() => runApp(MyApp());
 
+void _airDress() {
+
+}
+
+void _restitchDress() {
+
+}
+
+void _repairDress() {
+
+}
+
+// void navbar() {
+//   return AppBar(
+//     leading: Builder(
+//       builder: (BuildContext context) {
+//         return IconButton(
+//           icon: const Icon(Icons.menu),
+//           onPressed: () { Scaffold.of(context).openDrawer(); },
+//           tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+//         );
+//       },
+//     ),
+//     title: Text('My Fancy Dress'),
+//     actions: <Widget>[
+//       IconButton(
+//         icon: Icon(Icons.playlist_play),
+//         tooltip: 'Air it',
+//         onPressed: _airDress,
+//       ),
+//       IconButton(
+//         icon: Icon(Icons.playlist_add),
+//         tooltip: 'Restitch it',
+//         onPressed: _restitchDress,
+//       ),
+//       IconButton(
+//         icon: Icon(Icons.playlist_add_check),
+//         tooltip: 'Repair it',
+//         onPressed: _repairDress,
+//       ),
+//     ],
+//   );
+// }
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (choose the "Toggle Debug Paint" action
-          // from the Flutter Inspector in Android Studio, or the "Toggle Debug
-          // Paint" command in Visual Studio Code) to see the wireframe for each
-          // widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello, World!',
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        // appBar: AppBar(
+        //   title: Text('Welcome to Flutter lkfgj')
+        // ),
+        appBar: AppBar(
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: const Icon(Icons.menu),
+                onPressed: () { Scaffold.of(context).openDrawer(); },
+                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+              );
+            },
+          ),
+          title: Text('My Fancy Dress'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.playlist_play),
+              tooltip: 'Air it',
+              onPressed: _airDress,
+            ),
+            IconButton(
+              icon: Icon(Icons.playlist_add),
+              tooltip: 'Restitch it',
+              onPressed: _restitchDress,
+            ),
+            IconButton(
+              icon: Icon(Icons.playlist_add_check),
+              tooltip: 'Repair it',
+              onPressed: _repairDress,
             ),
           ],
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        // body: Text("DOOOM"),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('Map'),
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_album),
+              title: Text('Album'),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
+            ),
+          ],
+        ),
+      ),
+
     );
   }
 }
+
+// class MyBody extends StatefulWidget {
+//   @override
+//   Widget build(c){
+
+//   }
+// }
